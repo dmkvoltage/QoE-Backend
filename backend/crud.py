3c52ec47fa776c7c87efa2649020303237c98914
KINGO KINGSLEY KAAH <kranedmk@gmail.com>
2025-06-06 02:24:40 +0100

    Update crud.py

--- backend/crud.py
+++ backend/crud.py
@@ -18,6 +18,9 @@ def verify_password(plain_password, hashed_password):
 def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
 
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
 def create_user(db: Session, user: UserCreate):
     hashed_password = get_password_hash(user.password)
     db_user = User(
